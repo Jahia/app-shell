fbd13cc5ed0ce82831cc65f547eb69d7b5bb1001
FBerthelot <florent.berthelot35@gmail.com>
2020-03-20 15:13:15 +0100

    BACKLOG-12748: Set JahiaCommons public path

--- src/javascript/jahia.js
+++ src/javascript/jahia.js
@@ -1,7 +1,6 @@
-/* global jahiaCommons */
 // eslint-disable-next-line no-undef, camelcase
 __webpack_public_path__ = `${window.contextJsParameters.contextPath}/modules/app-shell/javascript/apps/`;
-jahiaCommons.p = `${window.contextJsParameters.contextPath}/modules/app-shell/javascript/commons/`;
+window.jahiaCommons.p = `${window.contextJsParameters.contextPath}/modules/app-shell/javascript/commons/`;
 
 // Configuration
 import './apollo/register';
