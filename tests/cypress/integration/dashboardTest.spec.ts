31c2f68f22bec50dcd3ade8d10e32c91fafb15e5
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-11-30 14:44:24 -0500

    Fixed typo

--- tests/cypress/integration/dashboardTest.spec.ts
+++ tests/cypress/integration/dashboardTest.spec.ts
@@ -1,7 +1,7 @@
 import { dashboard } from '../page-object/dashboard.page'
 
 describe('Navigation to the Dashboard', () => {
-    it('Verifies a Welcom and Documentation sections are present', function () {
+    it('Verifies a Welcome and Documentation sections are present', function () {
         dashboard.goTo()
         dashboard.getByText('p', 'Welcome').should('be.visible')
         dashboard.getByText('p', 'Documentation').should('be.visible')
