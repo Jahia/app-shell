f34b19c21cfd9e9a5a11599de038f7c51f6cf50c
Thomas Draier <tdraier@jahia.com>
2021-03-01 14:18:39 +0100

    BACKLOG-15679: Fixed remote init, cleaned compat (#177)

--- src/javascript/jahia.js
+++ src/javascript/jahia.js
@@ -1,4 +1,15 @@
-import('./bootstrap').then(res => {
-    window.jahia = res;
-    res.startAppShell(window.appShell);
+// eslint-disable-next-line no-undef
+__webpack_init_sharing__('default');
+
+console.log('Initializing remotes ..');
+Promise.all([
+    // eslint-disable-next-line no-undef, camelcase
+    ...Object.values(window.appShell.remotes || {}).map(container => container.init(__webpack_share_scopes__.default))
+]).then(() => {
+    console.log('Bootstrapping application ..');
+
+    import('./bootstrap').then(res => {
+        window.jahia = res;
+        res.startAppShell(window.appShell);
+    });
 });
