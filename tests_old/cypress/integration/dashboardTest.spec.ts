8f7a6e134052875164c0f7915e1bb7d1761338ad
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-07-22 09:53:04 -0400

    [jahia update signature] - Clean tests and force signature update

--- tests_old/cypress/integration/dashboardTest.spec.ts
+++
@@ -1,9 +0,0 @@
-import { dashboard } from '../page-object/dashboard.page'
-
-describe('Navigation to the Dashboard', () => {
-    it('Verifies a Welcome and Documentation sections are present', function () {
-        dashboard.goTo()
-        dashboard.getByText('p', 'Welcome').should('be.visible')
-        dashboard.getByText('p', 'Documentation').should('be.visible')
-    })
-})
--- tests_old/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
-
-module.exports = (on, config) => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("@cypress/code-coverage/task")(on, config);
-    on("file:preprocessor", cypressTypeScriptPreprocessor);
-    return config;
-};
