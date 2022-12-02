ef65ee2b216b7ce845586881fa283ea2bed3ab04
Thomas Draier <tdraier@jahia.com>
2022-12-02 18:03:13 +0100

    Fixed lint config (#241)

--- tests/cypress/integration/dashboardTest.spec.ts
+++ tests/cypress/integration/dashboardTest.spec.ts
@@ -1,10 +1,10 @@
-import { DashboardPage } from '../page-object/dashboard.page'
+import {DashboardPage} from '../page-object/dashboard.page';
 
 describe('Navigation to the Dashboard', () => {
     it('Verifies a Welcome and Documentation sections are present', function () {
-        cy.login()
-        const dashboard = DashboardPage.visit()
-        cy.contains('p', 'Welcome').should('be.visible')
-        cy.contains('p', 'Documentation').should('be.visible')
-    })
-})
+        cy.login();
+        DashboardPage.visit();
+        cy.contains('p', 'Welcome').should('be.visible');
+        cy.contains('p', 'Documentation').should('be.visible');
+    });
+});
