3364b9f51696f55c0666bdf4522dcde6dde08ed0
Francois G <fgerthoffert@jahia.com>
2023-11-21 11:31:43 -0500

    added debug data

--- tests/ci.startup.cloud.sh
+++ tests/ci.startup.cloud.sh
@@ -20,13 +20,20 @@ curl -i https://jahia.cloud/cms/render/live/en/sites/cloud/home.createJahiaEnv.d
     -d "envName=${CLOUD_ENVNAME}&shortDomain=${CLOUD_ENVNAME}-${CLOUD_DOMAINSUFFIX}&productVersion=${CLOUD_JAHIA_VERSION}&rootPassword=${SUPER_USER_PASSWORD}&subscriptionItemId=${CLOUD_SUBSCRIPTIONITEMID}&regionId=${CLOUD_REGIONID}&bandId=${CLOUD_BANDID}"
 
 echo " == Waiting for Jahia to startup"
-while [[ "$(curl -v -s -o /dev/null -w ''%{http_code}'' ${JAHIA_URL}/cms/login)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ${JAHIA_URL}/cms/login)" != "200" ]];
   do sleep 5;
   echo "---"
 done
 
+echo " == CURL CMS LOGIN before sleep"
+curl -i ${JAHIA_URL}/cms/login)
+
 echo "Sleeping for 60s"
 # To be removed
 sleep 60 
+
+echo " == CURL CMS LOGIN after sleep"
+curl -i ${JAHIA_URL}/cms/login)
+
 
 docker run --name cypress -e MANIFEST=${MANIFEST} -e SUPER_USER_PASSWORD=${SUPER_USER_PASSWORD} -e JAHIA_URL=${JAHIA_URL} -e NEXUS_USERNAME=${NEXUS_USERNAME} -e NEXUS_PASSWORD=${NEXUS_PASSWORD} ${TESTS_IMAGE}
