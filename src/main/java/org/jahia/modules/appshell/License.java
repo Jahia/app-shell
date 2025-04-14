01fe1bb8727d5fc25596098f90a6f79ff4133140
akarmanov <alexandrk_@hotmail.com>
2025-04-14 12:41:41 -0400

    fix: Fix date

--- src/main/java/org/jahia/modules/appshell/License.java
+++ src/main/java/org/jahia/modules/appshell/License.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c)  Jahia Solutions Group. All rights reserved.
+ * Copyright (c) 2002 - 2025 Jahia Solutions Group. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
